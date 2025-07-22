d2d6dc31d0d1e3581ca318dbd1aab5b706bcc380
meetkamal256 <meetkamal256@gmail.com>
2025-07-22 10:23:34 +0100

    chore: initialize React TypeScript project with Tailwind CSS v4 setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Tailwind v4 is working!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
